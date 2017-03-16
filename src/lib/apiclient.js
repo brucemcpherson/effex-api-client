bc73c6046cacd65ed4f1bdc0f949a6097bf7bea9
bruce mcpherson <bruce@mcpher.com>
2017-03-16 17:06:26 +0000

    first

---
+++ src/lib/apiclient.js
@@ -0,0 +1,272 @@
+/**
+ * need axios for http operations
+ * that should also bring in es6 which is needed anyway
+ */
+var axios = require('axios');
+
+/**
+ * the api client for Ephemeral exchange
+ */
+var api = (function(ns) {
+  "use strict";
+
+  // the api base url
+  let ax;
+  let admin;
+  let keys = {};
+
+  ns.setBase = function(base, adminKey) {
+    ax = axios.create({
+      baseURL: base
+    });
+    admin = adminKey;
+  };
+
+
+  /**
+   * turns a params object into a url
+   * @param {object} params the params
+   * @return {string} the uri
+   */
+  function makeParams(params) {
+    params = params || {};
+    const pa = Object.keys(params).reduce(function(p, c) {
+      p.push(`${c}=${encodeURIComponent(params[c])}`);
+      return p;
+    }, []);
+
+    return pa.length ? `?${pa.join("&")}` : "";
+  }
+
+  /**
+   * turns a params object into an a url + admin key 
+   * @param {object} params the params
+   * @return {string} the uri
+   */
+  function makeAdmin(params) {
+    return makeParams({...(params || {}),
+      admin: admin
+    });
+  }
+
+  /**
+   * @param {string} accountId the account id
+   * @param {string} planId the plan type
+   * @param {object} params the lifetime of the key in seconds
+   * @return {Promise} to the result
+   */
+  ns.generateBoss = function(accountId, planId, params) {
+    return ax.get(`/admin/account/${accountId}/boss/${planId}${makeAdmin(params)}`);
+  };
+
+  /**
+   * @param {string} boss the boss key
+   * @param {string} mode the type like writer/reader/updater
+   * @param {object} params the params 
+   * @return {Promise} to the result
+   */
+  ns.generateKey = function(boss, mode, params) {
+    return ax.get(`/${boss}/${mode}${makeParams(params)}`);
+  };
+
+  /**
+   * ping the service
+   * @return {string} "PONG"
+   */
+  ns.ping = function() {
+    return ax.get('/ping');
+  };
+
+  /**
+   * info the service
+   * @return {string} "PONG"
+   */
+  ns.info = function() {
+    return ax.get('/info');
+  };
+
+  ns.setKeys = function(pkeys) {
+    keys = pkeys;
+  };
+
+  ns.getKeys = function() {
+    return keys;
+  };
+
+
+  /**
+   * get quotas 
+   * @return {object} the quotas
+   */
+  ns.getQuotas = function() {
+    return ax.get('/quotas');
+  };
+
+  /**
+   * update an item
+   * @param {string} id the item id
+   * @param {string} updater the updater key
+   * @param {object} data what to write
+   * @param {string} method the to use (post,get)
+   * @param {object} params the params 
+   * @return {Promise} to the result
+   */
+  ns.update = function(data, id, updater, method  , params) {
+    
+    method = method || "post";
+    method = method.toLowerCase();
+    updater = updater || keys.updater;
+    params = params || {};
+
+    if (method === "get") {
+      params = {...params,
+        data: data
+      };
+      return ax.get(`/updater/${updater}/${encodeURIComponent(id)}${makeParams(params)}`);
+    }
+    else if (method === "post") {
+      return ax.post(`/updater/${updater}/${encodeURIComponent(id)}${makeParams(params)}`, {
+        data: data
+      });
+    }
+    else {
+      return Promise.reject("invalid method:" + method);
+    }
+
+  };
+
+  /**
+   * @param {string} writer the writer key
+   * @param {object} data what to write
+   * @param {string} method the to use (post,get)
+   * @param {object} params the params 
+   * @return {Promise} to the result
+   */
+  ns.write = function( data, writer, method, params) {
+    method = method || "POST";
+    writer = writer || keys.writer;
+    method = method.toLowerCase();
+    params = params || {};
+
+    if (method === "get") {
+      params = {...params,
+        data: JSON.stringify(data)
+      };
+      return ax.get(`/writer/${writer}${makeParams(params)}`);
+    }
+    else if (method === "post") {
+      return ax.post(`/writer/${writer}${makeParams(params)}`, {
+        data: data
+      });
+    }
+    else {
+      return Promise.reject("invalid method:" + method);
+    }
+
+  };
+
+  /**
+   * @param {string} id the item id
+   * @param {string} writer the writer key
+   * @param {object} params the params 
+   * @return {Promise} to the result
+   */
+  ns.remove = function(id, writer, params) {
+    params = params || {};
+    writer = writer || keys.writer;
+    return ax.delete(`/writer/${writer}/${encodeURIComponent(id)}${makeParams(params)}`);
+  };
+
+  /**
+   * @param {string} id the item id
+   * @param {string} reader the reader key
+   * @param {object} params the params 
+   * @return {Promise} to the result
+   */
+  ns.read = function(id, reader, params) {
+    params = params || {};
+    reader = reader || keys.reader;
+    return ax.get(`/reader/${reader}/${encodeURIComponent(id)}${makeParams(params)}`);
+  };
+
+  /**
+   * @param {string} coupon the coupon code
+   * @return {Promise} to the result
+   */
+  ns.validateKey = function(coupon) {
+    return ax.get(`/validate/${coupon}`);
+  };
+
+  /**
+   * @param {string} accountId the account id
+   * @param {string} authid the authid
+   * @param {boolean} active whether active
+   * @return {Promise} to the result
+   */
+  ns.registerAccount = function(accountId, authId, active) {
+    return ax.post(`/admin/register/${accountId}${makeAdmin()}`, {
+      data: {
+        authid: authId,
+        active: active
+      }
+    });
+  };
+  /**
+   * @param {string} accountId the account id
+   * @return {Promise} to the result
+   */
+  ns.removeAccount = function(accountId) {
+    return ax.delete(`/admin/remove/${accountId}${makeAdmin()}`);
+  };
+
+  /**
+   * @param {string} accountId the account id
+   * @return {Promise} to the result
+   */
+  ns.pruneBosses = function(accountId) {
+    return ax.delete(`/admin/prune/${accountId}${makeAdmin()}`);
+  };
+
+  /**
+   * @param {string} accountId the account id
+   * @return {Promise} to the result
+   */
+  ns.getBosses = function(accountId) {
+    return ax.get(`/admin/bosses/${accountId}${makeAdmin()}`);
+  };
+
+  /**
+   * @param {string} accountId the account id
+   * @param {object} params any parameters
+   * @return {Promise} to the result
+   */
+  ns.getStats = function(accountId, params) {
+    return ax.get(`/admin/stats/${accountId}${makeAdmin(params)}`);
+  };
+
+
+  /**
+   * @param {string} accountId the account id
+   * @return {Promise} to the result
+   */
+  ns.removeBosses = function(bossKeys) {
+    return ax.put(`/admin/bosses/${makeAdmin()}`, {
+      data: {
+        keys: bossKeys
+      }
+    });
+  };
+
+  ns.registerAlias = function(writer, key, id, alias, params) {
+    return ax.get(`/${writer}/${key}/alias/${encodeURIComponent(alias)}/${id}${makeParams(params)}`);
+  };
+
+
+
+  return ns;
+})({});
+
+module.exports = api;
+
+// Allow use of default import syntax in TypeScript
+module.exports.default = api;
---
+++ test/test.js
@@ -0,0 +1,437 @@
+var expect = require('chai').expect;
+
+
+var efx = require('../dist/index');
+
+// set up client 
+efx.setBase("https://ephex-auth.appspot-preview.com");
+
+// boss key comes from console /// replace this with your own
+var bossKey = "bx2ao-1gl-bfe63jxaod29";
+
+// service status tests
+describe('status', function() {
+
+  //return expect(Promise.resolve({ foo: "bar" })).to.eventually.have.property("foo");
+  //PING
+
+  it('ping', function() {
+
+    return efx.ping().then(function(result) {
+      expect(result.data).to.be.an('object');
+      expect(result.data.value).to.equal('PONG');
+      expect(result.data.code).to.equal(200);
+      expect(result.data.ok).to.equal(true);
+
+    });
+  });
+
+
+  //INFO
+  it('info', function() {
+    return efx.info().then(function(result) {
+      expect(result.data).to.be.an('object');
+      expect(result.data.info).to.have.all.keys('api', 'version');
+      expect(result.data.code).to.equal(200);
+      expect(result.data.ok).to.equal(true);
+    });
+  });
+
+
+});
+
+// Work with keys
+// we'll need these later
+var promises = {}, textData = "just some test data to write",alias ="somefunnyname", otherAlias = "anothername",
+  someData = {
+    name: 'xyz',
+    a: [1, 2, 3],
+    b: 2000
+  };
+
+describe('keys', function() {
+
+  //validate boss key
+
+  it('validate boss key not expired', function() {
+    return efx.validateKey(bossKey).then(function(result) {
+      expect(result.data).to.be.an('object');
+      expect(result.data.key).to.equal(bossKey);
+      expect(new Date(result.data.validtill).getTime()).to.be.above(new Date().getTime());
+      expect(result.data.ok).to.equal(true);
+    });
+  });
+
+  // get 1 writer key expiringin in 5 minutes
+  it('generate writer key', function() {
+    promises.writers = efx.generateKey(bossKey, "writer", {
+      seconds: 5 * 60
+    });
+    return promises.writers.then(function(result) {
+      var writers = result.data;
+      expect(writers).to.be.an('object');
+      expect(writers.keys).to.be.instanceof(Array);
+      expect(writers.keys.length).to.equal(1);
+      expect(writers.ok).to.equal(true);
+      expect(new Date(writers.validtill).getTime()).to.be.closeTo(1000 * 5 * 60 + new Date().getTime(), 2000);
+
+    });
+  });
+
+
+
+  // get 2 reader keys expiringin in 5 minutes
+  it('generate reader keys', function() {
+    promises.readers = efx.generateKey(bossKey, "reader", {
+      seconds: 5 * 60,
+      count: 2
+    });
+    return promises.readers.then(function(result) {
+      var readers = result.data;
+      expect(readers).to.be.an('object');
+      expect(readers.keys).to.be.instanceof(Array);
+      expect(readers.keys.length).to.equal(2);
+      expect(readers.ok).to.equal(true);
+      expect(new Date(readers.validtill).getTime()).to.be.closeTo(1000 * 5 * 60 + new Date().getTime(), 2000);
+    });
+  });
+
+
+
+  // get 1 update key expiring in 5 minutes
+
+  it('generate update key', function() {
+    promises.updaters = efx.generateKey(bossKey, "updater", {
+      seconds: 5 * 60,
+      count: 1
+    });
+    return promises.updaters.then(function(result) {
+      var updaters = result.data;
+      expect(updaters).to.be.an('object');
+      expect(updaters.keys).to.be.instanceof(Array);
+      expect(updaters.keys.length).to.equal(1);
+      expect(updaters.ok).to.equal(true);
+      expect(new Date(updaters.validtill).getTime()).to.be.closeTo(1000 * 5 * 60 + new Date().getTime(), 2000);
+    });
+
+  });
+});
+describe('items', function() {
+
+
+  it('should be keys', function() {
+
+    promises.keys = Promise.all([promises.writers, promises.readers, promises.updaters]).then(function(res) {
+      // set the keys up as default so we dont have to bother specifying them later
+
+      efx.setKeys({
+        updater: res[2].data.keys[0],
+        writer: res[0].data.keys[0],
+        reader: res[1].data.keys[0]
+      });
+      return efx.getKeys();
+    });
+
+    return promises.keys.then(function(keys) {
+      expect(keys).to.be.an('object');
+      expect(keys).to.have.all.keys('updater', 'writer', 'reader');
+    });
+  });
+
+  it('write post', function() {
+
+    return promises.keys
+      .then(function(keys) {
+        return promises.writePost = efx.write(someData);
+      })
+      .then(function(result) {
+        var data = result.data;
+        expect(data).to.be.an('object');
+        expect(data.ok).to.equal(true);
+        expect(data.code).to.equal(201);
+        expect(data.id).to.be.a('string');
+        expect(data.lifetime).to.equal(3600);
+      });
+  });
+
+  it('read post', function() {
+    return Promise.all([promises.keys, promises.writePost])
+      .then(function(res) {
+        var keys = res[0];
+        var data = res[1].data;
+        // just read with a writer key
+        return efx.read(data.id, keys.writer);
+      })
+      .then(function(result) {
+        expect(someData).to.deep.equal(result.data.value);
+      });
+  });
+
+
+  it('write with get', function() {
+
+    return promises.keys
+      .then(function(keys) {
+        return promises.writeGet = efx.write(someData,keys.writer, "GET");
+      })
+      .then(function(result) {
+        var data = result.data;
+        expect(data).to.be.an('object');
+        expect(data.ok).to.equal(true);
+        expect(data.code).to.equal(201);
+        expect(data.id).to.be.a('string');
+        expect(data.lifetime).to.equal(3600);
+      });
+  });
+
+  it('read write with get', function() {
+    return Promise.all([promises.keys, promises.writeGet])
+      .then(function(res) {
+        var keys = res[0];
+        var data = res[1].data;
+        // just read with a writer key
+        return efx.read(data.id, keys.writer);
+      })
+      .then(function(result) {
+        expect(someData).to.deep.equal(result.data.value);
+      });
+  });
+  
+  it('write text', function() {
+
+    return promises.keys
+      .then(function(keys) {
+        return promises.writeText = efx.write(textData);
+      })
+      .then(function(result) {
+        var data = result.data;
+        expect(data).to.be.an('object');
+        expect(data.ok).to.equal(true);
+        expect(data.code).to.equal(201);
+        expect(data.id).to.be.a('string');
+        expect(data.lifetime).to.equal(3600);
+      });
+  });
+
+  it('read text', function() {
+    return Promise.all([promises.keys, promises.writeText])
+      .then(function(res) {
+        var keys = res[0];
+        var data = res[1].data;
+        // just read with a writer key
+        return efx.read(data.id, keys.writer);
+      })
+      .then(function(result) {
+        expect(textData).to.equal(result.data.value);
+      });
+  });
+
+});
+describe('sharing items', function() {
+
+
+  it('assigning updaters and readers', function() {
+
+    return promises.keys
+      .then(function(keys) {
+        return promises.assigning = efx.write(someData,keys.writer,"post",{readers:keys.reader,updaters:keys.updater});
+      })
+      .then(function(result) {
+        expect(result.data.ok).to.equal(true);
+      });
+  });
+
+  it('read with reader key', function() {
+    return Promise.all([promises.keys, promises.assigning])
+      .then(function(res) {
+        var keys = res[0];
+        var data = res[1].data;
+        return efx.read(data.id, keys.reader);
+      })
+      .then(function(result) {
+        expect(someData).to.deep.equal(result.data.value);
+      });
+  });
+
+  it('read with updater key', function() {
+    return Promise.all([promises.keys, promises.assigning])
+      .then(function(res) {
+        var keys = res[0];
+        var data = res[1].data;
+        return efx.read(data.id, keys.updater);
+      })
+      .then(function(result) {
+        expect(someData).to.deep.equal(result.data.value);
+      });
+  });
+  
+  it('update', function() {
+    return Promise.all([promises.keys, promises.assigning])
+      .then(function(res) {
+        var data = res[1].data;
+        return promises.updateItem = efx.update(textData, data.id);
+      })
+      .then(function(result) {
+        expect(result.data.ok).to.equal(true);
+      });
+  });
+
+  it('read updated item', function() {
+    return Promise.all([promises.keys, promises.updateItem])
+      .then(function(res) {
+        var data = res[1].data;
+        return efx.read(data.id);
+      })
+      .then(function(result) {
+        expect(textData).to.deep.equal(result.data.value);
+      });
+  });
+
+
+
+});
+
+describe('working with aliases', function() {
+
+    it('registering', function() {
+
+      return Promise.all([promises.keys,promises.updateItem]).then (function (res) {
+        var keys = res[0];
+        var data = res[1].data;
+        return promises.alias = efx.registerAlias( keys.writer, keys.reader, data.id, alias);
+      })
+      .then (function (result) {
+        var data = result.data;
+        expect(data).to.be.an('object');
+        expect(data.ok).to.equal(true);
+        expect(data.code).to.equal(201);
+        expect(data.id).to.be.a('string');
+        expect(data.alias).to.equal(alias);
+        expect(new Date(data.validtill).getTime()).to.be.closeTo(1000 * 5 * 60 + new Date().getTime(), 10000);
+      });
+    });
+  
+    it('read alias with reader key', function() {
+      return Promise.all([promises.keys, promises.alias])
+        .then(function(res) {
+          var keys = res[0];
+          var data = res[1].data;
+          return efx.read(data.alias, keys.reader);
+        })
+        .then(function(result) {
+          expect(textData).to.equal(result.data.value);
+        });
+    });
+    
+    it('write another with same alias', function() {
+      return Promise.all([promises.keys, promises.alias])
+        .then(function(res) {
+          var keys = res[0];
+          return promises.otherWrite = efx.write(someData,keys.writer,"post",{readers:keys.reader,updaters:keys.updater});
+        })
+        .then(function(result) {
+          expect(result.data.ok).to.equal(true);
+        });
+    });
+
+    it('registering other alias', function() {
+
+      return Promise.all([promises.keys,promises.otherWrite]).then (function (res) {
+        var keys = res[0];
+        var data = res[1].data;
+        return promises.otherAlias = efx.registerAlias( keys.writer, keys.updater, data.id, otherAlias);
+      })
+      .then (function (result) {
+        var data = result.data;
+        expect(data.alias).to.equal(otherAlias);
+      });
+    });
+
+    it('read other alias with updater key', function() {
+      return Promise.all([promises.keys, promises.otherAlias])
+        .then(function(res) {
+          var keys = res[0];
+          var data = res[1].data;
+          return efx.read(data.alias, keys.updater);
+        })
+        .then(function(result) {
+          expect(someData).to.deep.equal(result.data.value);
+        });
+    });
+  
+    it('update other alias', function() {
+      return Promise.all([promises.keys, promises.otherAlias])
+        .then(function(res) {
+          var data = res[1].data;
+          return efx.update(textData , data.alias);
+        })
+        .then(function(result) {
+          expect(result.data.ok).to.equal(true);
+        });
+    });
+
+    it('read updated other alias with updater key', function() {
+      return Promise.all([promises.keys, promises.otherAlias])
+        .then(function(res) {
+          var keys = res[0];
+          var data = res[1].data;
+          return promises.readUpdateOtherAlias = efx.read(data.alias, keys.updater);
+        })
+        .then(function(result) {
+          expect(textData).to.equal(result.data.value);
+        });
+    });
+    
+    it('delete should fail because alias not assigned to writer key', function() {
+      return Promise.all([promises.keys, promises.readUpdateOtherAlias])
+        .then(function(res) {
+          var data = res[1].data;
+          return efx.remove(data.alias);
+        })
+        .then(function(result) {
+          expect(result.data.ok).to.equal(false);
+          expect(result.data.code).to.equal(404);
+        });
+    });
+
+    it('registering other alias to writer key', function() {
+
+      return Promise.all([promises.keys,promises.readUpdateOtherAlias]).then (function (res) {
+        var keys = res[0];
+        var data = res[1].data;
+        return promises.otherWriterAlias = efx.registerAlias( keys.writer, keys.writer, data.id, otherAlias);
+      })
+      .then (function (result) {
+        var data = result.data;
+        expect(data.alias).to.equal(otherAlias);
+      });
+    });
+  
+    it('delete should work because alias is now assigned to writer key', function() {
+      return Promise.all([promises.keys, promises.otherWriterAlias])
+        .then(function(res) {
+          var data = res[1].data;
+          return promises.remove = efx.remove(data.alias);
+        })
+        .then(function(result) {
+          expect(result.data.ok).to.equal(true);
+          expect(result.data.code).to.equal(204);
+        });
+    });
+
+    it('check its gone', function() {
+      return Promise.all([promises.keys, promises.remove, promises.readUpdateOtherAlias])
+        .then(function(res) {
+          var keys = res[0];
+          var data = res[2].data;
+          return efx.read(data.id,keys.writer);
+        })
+        .then(function(result) {
+          expect(result.data.ok).to.equal(false);
+          expect(result.data.code).to.equal(404);
+        });
+    });
+ 
+  
+});
+
