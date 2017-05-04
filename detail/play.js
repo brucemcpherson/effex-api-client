fb7f93c74856a3c0269492c3fd2adbc6b90e8850
bruce mcpherson <bruce@mcpher.com>
2017-05-04 12:45:02 +0000

    removed config

--- detail/play.js
+++
@@ -1,7 +0,0 @@
-var Socketing = require('../src/lib/socketing');
-var config = require('../src/lib/config.js');
-var sk = new Socketing(config.dev);
-sk.connect().then (function () {
-  console.log(sk.isConnected());
-});
-
---
+++ src/lib/yourconfig.js
@@ -0,0 +1,21 @@
+/**
+ * parameters to identify api defaults - see me for what this should be
+ */
+module.exports = {
+
+  prod: {
+    base:'',
+    basePort:'',
+    socketBase:'',
+    socketPort:'',
+    socketPass :""
+  },
+  dev: {
+    base:'',
+    basePort:'',
+    socketBase:'',
+    socketPort:'',
+    socketPass: ""
+  }
+
+};
